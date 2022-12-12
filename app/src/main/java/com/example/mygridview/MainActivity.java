6db516e618747d0025c94641a3610a4c2f0838f5
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-12-12 23:24:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mygridview/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.mygridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+    ArrayList<String> text = new ArrayList<>();
+    ArrayList<Integer> image = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridView);
+        fillArray();
+        GridAdapter adapter = new GridAdapter(this, text, image);
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, text.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private void fillArray() {
+        text.add("Baguio's Night Market");
+        text.add("Batanes Island");
+        text.add("Magellan's Cross, Cebu");
+        text.add("Chocolate Hills, Bohol");
+        text.add("Manila Cathedral, Manila");
+        text.add("Mayon Volcano, Albay");
+        text.add("Island in Coron, Palawan");
+        text.add("Mt. Pinatubo, Zambales");
+        text.add("Banaue Rice Terraces, Ifugao");
+        text.add("Taal Volcano, Batangas");
+        text.add("U.P. Diliman, Quezon City");
+        text.add("Vigan's old houses");
+
+//        Images
+        image.add(R.drawable.baguio);
+        image.add(R.drawable.batanes);
+        image.add(R.drawable.cebu);
+        image.add(R.drawable.chocohills);
+        image.add(R.drawable.intramuros);
+        image.add(R.drawable.palawan);
+        image.add(R.drawable.pinatubo);
+        image.add(R.drawable.riceterraces);
+        image.add(R.drawable.mayon);
+        image.add(R.drawable.taal);
+        image.add(R.drawable.upd);
+        image.add(R.drawable.vigan);
+    }
+
+}
---
+++ app/src/main/java/com/example/mygridview/Textview.java
@@ -0,0 +1,4 @@
+package com.example.mygridview;
+
+public class Textview {
+}
